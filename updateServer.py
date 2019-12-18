2eab47061aa8bbcb65d6c2ec80053025cf64efac
davidcarl <david.martin.carl@gmail.com>
2019-12-18 21:41:24 +0100

    updated script

--- updateServer.py
+++ updateServer.py
@@ -14,14 +14,13 @@ class Webhook(Resource):
 
     def post(self):
         json_data = request.get_json(force=True)
-        print(json_data)
         repo_name = json_data['repository']['repo_name']
         tag = json_data['push_data']['tag']
         print(repo_name + ':' + tag)
-        stoppingContainer(repo_name)
-        startContainer(repo_name, tag)
+        if tag != 'latest':
+            stoppingContainer(repo_name)
+            startContainer(repo_name, tag)
         return {"status":"ok"}
-
 
 api.add_resource(Webhook, '/')
 
@@ -31,12 +30,11 @@ def startContainer(imageName, tag):
     elif imageName == 'davidcarl/si-mp3-frontend':
         subprocess.Popen(['docker','run','-d','-p','80:3000', imageName + ':' + tag])
     elif imageName == 'davidcarl/si-mp3-backend':
-        subprocess.Popen(['docker','run','-d','-p','88:3000', imageName + ':' +tag])
+        subprocess.Popen(['docker','run','-d','-p','88:3001', imageName + ':' +tag])
 
 def stoppingContainer(imageName):
     containers = client.containers.list()
     for each in containers:
-        print(each.image.attrs)
         name = each.image.attrs['RepoDigests'][0].split('@')[0]
         if name == imageName:
             print('Stopping id:', each.id)
